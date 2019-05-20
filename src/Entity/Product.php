96c55f129a1bfd2062912f97a985e580cbd430c2
arkadiypr <arkadij.pryadko@gmail.com>
2019-05-20 15:59:30 +0300

    Liip Image Bundle

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -237,7 +237,7 @@ class Product
         return $this->imageName;
     }
 
-    public function setImageName(string $imageName): self
+    public function setImageName(?string $imageName): self
     {
         $this->imageName = $imageName;
 
